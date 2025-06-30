0e73788ad94d0085aedf95021eaa1b844cb3288d
rifqyfauzan9 <r1fqyf4uz4n@gmail.com>
2025-06-30 20:08:39 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello, World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est numquam suscipit nisi recusandae, sunt voluptate quae nostrum placeat magni architecto quibusdam, iste impedit optio alias quam eligendi cupiditate, aut reiciendis?</p>
 </body>
 </html>
